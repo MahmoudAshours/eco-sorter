25946e6e21e1be212afb139c7b2495f082b2255b
Mahmoud Ashour <50237142+MeitanteiAshour@users.noreply.github.com>
2020-12-26 23:54:23 +0200

    Update and rename app.py to flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -5,21 +5,12 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import torch
 import torch.nn.functional as F
-from torchvision.datasets import ImageFolder
 from PIL import Image
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
 
-def predict_image(img, model):
-    # Convert to a batch of 1
-    xb = to_device(img.unsqueeze(0), device)
-    # Get predictions from model
-    yb = model(xb)
-    # Pick index with highest probability
-    prob, preds = torch.max(yb, dim=1)
-    # Retrieve the class label
-    return dataset.classes[preds[0].item()]
 
 
 def accuracy(outputs, labels):
@@ -60,53 +51,29 @@ class ResNet(ImageClassificationBase):
         self.network = models.resnet50(pretrained=True)
         # Replace last layer
         num_ftrs = self.network.fc.in_features
-        self.network.fc = nn.Linear(num_ftrs, len(dataset.classes))
+        self.network.fc = nn.Linear(num_ftrs, 4)
 
     def forward(self, xb):
         return torch.sigmoid(self.network(xb))
 
 
-def to_device(data, device):
-    """Move tensor(s) to chosen device"""
-    if isinstance(data, (list, tuple)):
-        return [to_device(x, device) for x in data]
-    return data.to(device, non_blocking=True)
+
+def compute(external_image):
+    model = ResNet()
+    checkpoint = torch.load('/home/flutterpython/mysite/datasets/new_torch.zip', map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint)
+    trans = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
+    image = Image.open(external_image)
+    input = trans(image)
+# ['paper', 'metal', 'plastic', 'glass']
+    input = input.view(1, 3, 256, 256)
+    output = model(input)
+    prediction = int(torch.max(output.data, 1)[1].numpy())
+    return prediction
 
 
-def get_default_device():
-    """Pick GPU if available, else CPU"""
-    if torch.cuda.is_available():
-        return torch.device('cuda')
-    else:
-        return torch.device('cpu')
 
-
-def predict_external_image(image_name):
-    image = Image.open(image_name)
-
-    example_image = transformations(image)
-    return predict_image(example_image, model)
-
-
-transformations = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
-
-data_dir = 'datasets/garbage classification/Garbage classification'
-
-dataset = ImageFolder(data_dir, transform=transformations)
-device = get_default_device()
-
-model = ResNet()
-model.load_state_dict(torch.load('datasets/new_torch.zip'))
-
-if torch.cuda.is_available():
-    model.cuda()
-
-
-@app.route('/', methods=['POST'])
+@app.route('/API', methods=['POST'])
 def eco_sorter():
     image = request.files['file']
-    return predict_external_image(image)
-
-
-if __name__ == '__main__':
-    app.run()
+    return str(compute(image))
